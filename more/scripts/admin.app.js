45f5493b08ea468d09930497a3b993bdd2e89cc6
siku <162820660+sikuentertaiment@users.noreply.github.com>
2024-03-23 22:41:08 +0700

    Update admin.app.js

--- more/scripts/admin.app.js
+++ more/scripts/admin.app.js
@@ -1109,10 +1109,11 @@ const app = {
 				await firebase.database().ref(`/orders/${this.data.resi}`).set(this.data);
 
 				let message = '';
-				message += 'Hallo Kak '+this.data.name+'.\n\n';
-				message += 'Kami sudah memperbarui data pesanan, mohon segera dicek.\n';
+				message += 'Hallo '+this.data.name+'\n\n';
+				message += 'Kami sudah memperbarui data pesanan\n';
+				message += 'Cek status pesanan kamu,\n\n';
 				message += 'Nomor Resi: *'+this.data.resi+'*\n\n';
-				message += 'Salam, FfStudio.';
+				message += 'Salam, AutoFocusServiceCamera.';
 				const fonnteResult = await app.sendWaNotif(this.data.number,message);
 				if(!fonnteResult.status)
 					alert(`Gagal Mengirim Pesan WA! ${fonnteResult.reason}`);
